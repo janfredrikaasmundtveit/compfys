8e122e38411d08d798edf16fa3f7806164a5af0e
janfredrikaasmundtveit <j.f.aasmundtveit@fys.uio.no>
2018-08-26 12:40:08 +0200

    not working

--- 2_2.cpp
+++ 2_2.cpp
@@ -1,9 +1,23 @@
 #include<cstdlib>
 #include<iostream>
 #include<cmath>
-int main(int n){
+int main(){
+double tup;
+int k=5;
+double tdown;
+double sup[k];
+for(int i=1;i<=k;i++){
+tup=1.0/(1.0+(i-1)*1.0);
+sup[i-1]=tup;
+//printf("%f\n",tup);
+}
 
+double tsup=0;
+for(int i=1;i<=k;i++){
+tsup=tsup+sup[i-1];
+}
+printf("%f\n",tsup);
 
+return 0;
 
-	
 }
