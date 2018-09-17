e9c4eee12f886c8f59419dc32e4dd60c5d6b3fdc
janfredrikaasmundtveit <j.f.aasmundtveit@fys.uio.no>
2018-09-17 13:55:56 +0200

    gives changed matrix, wrong though

---
+++ project_1_b.cpp
@@ -0,0 +1,88 @@
+//project 1
+//project 1
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <time.h>
+// use namespace for output and input
+using namespace std;
+ 
+ ofstream ofile;
+// Functions used
+inline double f(double x){return 100.0*exp(-10.0*x);
+}
+inline double exact(double x) {return 1.0-(1-exp(-10))*x-exp(-10*x);}
+
+
+ int main(int argc, char *argv[])
+ {
+ 	clock_t tStart = clock();
+ 	// creating a file containing number of intergrationpoints.
+	int n; 
+    string filename;
+    int exponent;
+    if( argc <= 1 ){
+          cout << "Bad Usage: " << argv[0] <<
+              " read also file name on same line and max power 10^n" << endl;
+          exit(1);
+    }
+        else{
+        filename = argv[1]; // first command line argument after name of program
+        exponent = atoi(argv[2]);
+}
+		for (int i = 1; i <= exponent; i++){
+      int n = (int) pow(10.0,i);
+      // Declare new file name
+      string fileout = filename;
+      // Convert the power 10^i to a string
+      string argument = to_string(i);
+      // Final filename as filename-i-
+      fileout.append(argument);
+      double h = 1.0/(n);
+      double hh = h*h;
+      // Set up arrays a lower off diagonal, b main diagonal, c upper of diagonal, u unknown, g modifid f(x)
+      double *b = new double [n+1]; double *c=new double [n+1]; double *a = new double [n+1]; double *u = new double [n+1];
+      double *x = new double[n+1]; double *g =new double [n+1];
+    
+      u[0] = u[n] = 0.0; // boundary conditions
+      b[0] = b[n] = 2; // fixed elements of diagonal
+     
+      for (int i = 0; i <= n; i++){
+		x[i]= i*h;
+        g[i] = hh*f(i*h); //1*n flop
+        b[i]=2; // defining the matix (this is stupid)
+        a[i]=-1;
+        c[i]=-1;
+      }
+       	// gaussian eliminaton
+	 for (int i = 1; i < n; i++) {
+		b[i]=b[i]-((a[i]*c[i-1])/b[i-1]); // back sub
+		g[i]=g[i]-((a[i]*g[i-1])/b[i-1]); //6n flops
+      } 
+
+       u[n-1] = g[n-1]/b[n-1];
+      for (int i = n-2; i > 0; i--){
+        u[i] = (g[i]-(c[i]*u[i+1]))/b[i]; // forward sub //3n flops
+}
+ 	// making file (not makefile) 
+  	 
+      ofile.open(fileout);
+      ofile << setiosflags(ios::showpoint | ios::uppercase);
+      //      ofile << "       x:             approx:          exact:       relative error" << endl;
+      for (int i = 0; i < n;i++) {
+	double xval = x[i];
+ 	 double RelativeError = fabs((exact(xval)-u[i])/exact(xval));
+         ofile << setw(15) << setprecision(8) << xval;
+         ofile << setw(15) << setprecision(8) << u[i];
+         ofile << setw(15) << setprecision(8) << exact(xval);
+         ofile << setw(15) << setprecision(8) << log10(RelativeError) << endl;
+      }
+    //  ofile << setw(15) << setprecision(8) <<  (double)(clock() - tStart)/CLOCKS_PER_SEC << endl;
+      ofile.close();
+	  	delete [] a; delete [] b; delete [] c;  delete [] u; delete [] x; delete [] g;
+ 	}
+ 	 cout << (double)(clock() - tStart)/CLOCKS_PER_SEC << endl;
+ 	return 0;
+ }
---
+++ project_2/project2_b.cpp
@@ -0,0 +1,187 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <time.h>
+//#include <armadillo>
+// use namespace for output and input
+using namespace std;
+//using namespace arma;
+void Jacobi_rotate ( double**, double**, int, int, int);
+double offdiag(double**, int*, int*,int);
+void DeallocateMatrix(double **, int, int);
+double ** AllocateMatrix(int, int);
+
+int main(int argc, char *argv[])
+{
+  int n=atoi(argv[1]);
+  double **A=AllocateMatrix(n,n);
+  double **R=AllocateMatrix(n,n);
+  int maxiter=100;
+  double tolerance = 1.0E-10; 
+double maxnondiag=1.0;
+int iterations = 0;
+for (int i = 0; i < n; i++)
+{for (int j = 0; j < n; j++)
+{
+  if (i==j)
+  {
+    A[i][j]=-2.0;
+    R[i][j]=1.0;
+  }
+  else if(i==j+1 || i==j-1){
+    A[i][j]=1.0;
+   
+  }
+  else {
+
+    A[i][j]=0.0;
+     }
+}
+
+}
+/*for (int i = 0; i < n; i++)
+{ cout << endl;
+  for (int j = 0; j < n; j++)
+{
+  
+cout << A[i][j] << '|';  
+}}*/
+
+while ( maxnondiag > tolerance && iterations <= maxiter)
+{
+   int p, q;
+   maxnondiag= offdiag(A, &p, &q, n);
+   Jacobi_rotate(A, R, p, q, n);
+   iterations++;
+}
+
+for (int i = 0; i < n; i++)
+{ cout << endl;
+  for (int j = 0; j < n; j++)
+{
+  
+cout << A[i][j] << '|';  
+}}
+
+for (int i = 0; i < n; i++)
+{ cout << endl;
+  for (int j = 0; j < n; j++)
+{
+  
+cout << R[i][j] << '|';  
+}}
+
+//test RAR^T=A
+/*
+double **B=AllocateMatrix(n,n);
+for (int i = 0; i < n; i++)
+{for (int j = 0; j < n; j++)
+{for (int l = 0; l < n; l++)
+{for (int k = 0; k < n; k++)
+{
+  B[i][j]=R[i][l]*A[l][k]*R[j][k];
+}}}}
+
+for (int i = 0; i < n; i++)
+{ cout << endl;
+  for (int j = 0; j < n; j++)
+{
+  
+cout << B[i][j] << '|';  
+}}*/
+
+cout << endl;
+cout << iterations;
+DeallocateMatrix(A,n,n);
+DeallocateMatrix(R,n,n);
+
+return 0;
+}
+
+void DeallocateMatrix(double ** Matrix, int m, int n){
+  for(int i=0;i<m;i++)
+    delete[] Matrix[i];
+  delete[] Matrix;
+}
+
+
+double ** AllocateMatrix(int m, int n){
+  double ** Matrix;
+  Matrix = new double*[m];
+  for(int i=0;i<m;i++){
+    Matrix[i] = new double[n];
+    for(int j=0;j<m;j++)
+      Matrix[i][j] = 0.0;
+  }
+  return Matrix;
+}
+
+void Jacobi_rotate ( double **A, double **R, int k, int l, int n ){
+  double s, c;
+  if ( A[k][l] != 0.0 ) {
+    double t, tau;
+    tau = (A[l][l] - A[k][k])/(2*A[k][l]);
+    
+    if ( tau >= 0 ) {
+      t = 1.0/(tau + sqrt(1.0 + tau*tau));
+    } else {
+      t = -1.0/(-tau +sqrt(1.0 + tau*tau));
+    }
+    
+    c = 1/sqrt(1+t*t);
+    s = c*t;
+  }
+   else {
+    c = 1.0;
+    s = 0.0;
+  }
+  double a_kk, a_ll, a_ik, a_il, r_ik, r_il;
+  a_kk = A[k][k];
+  a_ll = A[l][l];
+  A[k][k] = c*c*a_kk - 2.0*c*s*A[k][l] + s*s*a_ll;
+  A[l][l] = s*s*a_kk + 2.0*c*s*A[k][l] + c*c*a_ll;
+  A[k][l] = 0.0;  // hard-coding non-diagonal elements by hand
+  A[l][k] = 0.0;  // same here
+  for ( int i = 0; i < n; i++ ) {
+    if ( i != k && i != l ) {
+      a_ik = A[i][k];
+      a_il = A[i][l];
+      A[i][k] = c*a_ik - s*a_il;
+      A[k][i] = A[i][k];
+      A[i][l] = c*a_il + s*a_ik;
+      A[l][i] = A[i][l];
+    }
+//  And finally the new eigenvectors
+    r_ik = R[i][k];
+    r_il = R[i][l];
+
+    R[i][k] = c*r_ik - s*r_il;
+    R[i][l] = c*r_il + s*r_ik;
+  }
+  return;
+}
+
+//  the offdiag function, using Armadillo
+double offdiag(double **A, int* p, int* q, int n)
+{ //p=new int; q=new int;
+   int a,b;
+   double max;
+   for (int i = 0; i < n; ++i)
+   {
+       for ( int j = i+1; j < n; ++j)
+       {
+           double aij = fabs(A[i][j]);
+           if ( aij > max)
+           { 
+              max = aij;  a = i; b = j;
+           }
+       }
+   }
+   *p=a; *q=b;
+   return max;
+}
+
+
+
