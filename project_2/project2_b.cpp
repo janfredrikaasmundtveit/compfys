0b7e1b48d6cd30da5d8292852442ba447905d053
janfredrikaasmundtveit <j.f.aasmundtveit@fys.uio.no>
2018-09-19 16:00:40 +0200

    CODE WORKS

--- project_2/project2_b.cpp
+++ project_2/project2_b.cpp
@@ -14,12 +14,15 @@ void Jacobi_rotate ( double**, double**, int, int, int);
 double offdiag(double**, int*, int*,int);
 void DeallocateMatrix(double **, int, int);
 double ** AllocateMatrix(int, int);
+double ** MatrixMultiplication(double **, double **, int);
+
 
 int main(int argc, char *argv[])
 {
   int n=atoi(argv[1]);
   double **A=AllocateMatrix(n,n);
   double **R=AllocateMatrix(n,n);
+  double **C=AllocateMatrix(n,n);
   int maxiter=100;
   double tolerance = 1.0E-10; 
 double maxnondiag=1.0;
@@ -31,6 +34,7 @@ for (int i = 0; i < n; i++)
   {
     A[i][j]=-2.0;
     R[i][j]=1.0;
+    C[i][j]=1.0;
   }
   else if(i==j+1 || i==j-1){
     A[i][j]=1.0;
@@ -50,16 +54,29 @@ for (int i = 0; i < n; i++)
   
 cout << A[i][j] << '|';  
 }}*/
-
+double sum, rij;
 while ( maxnondiag > tolerance && iterations <= maxiter)
-{
+{ for (int i = 0; i < n; i++)
+{for (int j = 0; j < n; j++)
+{ C[i][j]=R[i][j];
+ if (i==j)
+  {
+    R[i][j]=1.0;
+  } 
+  else{
+    R[i][j]=0.0;
+  }
+}}
+  
    int p, q;
    maxnondiag= offdiag(A, &p, &q, n);
    Jacobi_rotate(A, R, p, q, n);
    iterations++;
-   cout<< p <<','<< q << endl; 
+   R=MatrixMultiplication(R,C,n);
+    cout<< p <<','<< q << endl; 
 }
 
+/*
 for (int i = 0; i < n; i++)
 { cout << endl;
   for (int j = 0; j < n; j++)
@@ -67,7 +84,7 @@ for (int i = 0; i < n; i++)
   
 cout << A[i][j] << '|';  
 }}
-
+cout << endl;
 for (int i = 0; i < n; i++)
 { cout << endl;
   for (int j = 0; j < n; j++)
@@ -76,29 +93,31 @@ for (int i = 0; i < n; i++)
 cout << R[i][j] << '|';  
 }}
 
-//test RAR^T=A
+//test R^TBR=A
 /*
 double **B=AllocateMatrix(n,n);
 for (int i = 0; i < n; i++)
 {for (int j = 0; j < n; j++)
-{for (int l = 0; l < n; l++)
-{for (int k = 0; k < n; k++)
-{
-  B[i][j]=R[i][l]*A[l][k]*R[j][k];
-}}}}
-
+{C[i][j]=R[j][i];
+}
+}
+B=MatrixMultiplication(A,R,n);
+B=MatrixMultiplication(C,B,n);
 for (int i = 0; i < n; i++)
 { cout << endl;
   for (int j = 0; j < n; j++)
 {
   
 cout << B[i][j] << '|';  
-}}*/
+}}
+DeallocateMatrix(B,n,n);
+*/
 
 cout << endl;
 cout << iterations;
 DeallocateMatrix(A,n,n);
 DeallocateMatrix(R,n,n);
+DeallocateMatrix(C,n,n);
 
 return 0;
 }
@@ -119,6 +138,18 @@ double ** AllocateMatrix(int m, int n){
       Matrix[i][j] = 0.0;
   }
   return Matrix;
+}
+
+double ** MatrixMultiplication(double ** a, double **b, int n){
+  double **c = AllocateMatrix(n, n);
+  for(int i=0;i < n;i++){
+     for (int j=0 ; j < n;j++){
+       double sum = 0.0;
+       for (int k = 0; k < n; k++) sum += a[i][k]*b[k][j];
+       c[i][j] = sum;
+     }
+  }
+  return c;
 }
 
 void Jacobi_rotate ( double **A, double **R, int k, int l, int n ){
@@ -156,13 +187,16 @@ void Jacobi_rotate ( double **A, double **R, int k, int l, int n ){
       A[i][l] = c*a_il + s*a_ik;
       A[l][i] = A[i][l];
     }
-//  And finally the new eigenvectors
-    r_ik = R[i][k];
-    r_il = R[i][l];
 
-    R[i][k] = c*r_ik - s*r_il;
-    R[i][l] = c*r_il + s*r_ik;
+//  And finally the new eigenvectors
+    //r_ik = R[i][k];
+    //r_il = R[i][l];
   }
+    R[l][k] = s;
+    R[k][l] =- s;
+    R[k][k]=c;
+    R[l][l]=c;
+  
   return;
 }
 
